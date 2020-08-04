9f9ecd4458eb2561f3ca1674cb48e43249681192
RAMZIL <ramzilhuda1986@gmail.com>
2020-08-04 07:28:45 +0700

    first commit

---
+++ lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_navigasi_page/second_page.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("GO TO SECOND PAGE"),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SecondPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
